fa5001f7bffeaa2be0a4c75b41767693e9e228b4
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-05-15 11:17:11 -0700

    Fixes positioning of weather and time elements. Adds the bottom ticker without animation.

--- js/index.html.js
+++ js/index.html.js
@@ -1,5 +1,18 @@
-function getTime(){
-  let today = new Date();
-  time = document.getElementById('time')
-  time.innerHTML+= time;
+document.addEventListener("DOMContentLoaded", () => {
+  getTheTime();
+})
+
+function getTheTime() {
+  let currentTime = new Date();
+  let hour = addZero(currentTime.getHours());
+  let minutes = addZero(currentTime.getMinutes());
+  document.getElementById('time').innerHTML = `${hour} : ${minutes}`
+  setInterval(getTheTime, 1000);
+}
+
+function addZero(i) {
+  if (i < 10 ) {
+    i = "0" + i;
+  }
+  return i;
 }
